785e4a88a7bc103252ba25a86822764edfe8f214
flutter30 <janvimangukiya3008@gmail.com>
2022-12-11 11:45:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter Styling ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "text ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in Flutter ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
